3a9a93104d8d0ff78c8479c7b21ad25ee7b14794
Titusz <titusz.boros@student.upt.ro>
2024-12-10 20:23:21 +0200

    separated the projects

--- Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
+++ Lab2/obj/Debug/net8.0/Lab2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23adedc2f680af3800b62ababcb00168058d6414")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd942bdac12c8ca4a2cb3b1eb6c356ae9e1b893e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
